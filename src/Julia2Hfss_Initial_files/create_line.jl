6d971cd110aad6128691e63346d01bf4b98f22b8
MertTamer <merttamerkisisel@gmail.com>
2023-12-09 17:28:28 +0300

    create_line

---
+++ src/Julia2Hfss_Initial_files/create_line.jl
@@ -0,0 +1,48 @@
+module create_line 
+    export Cline
+    using LaTeXStrings  
+    function Cline(ProjectName::String, PackagePath::String, LineName::String, X::Vector, Y::Vector)
+    
+        PointsNumber = length(X);
+        file = open("$(PackagePath)\\Julia2Hfss.jl\\src\\Julia2Hfss_Initial_files\\create_line.vbs", "w")
+        write(file, "Dim oAnsoftApp\n")
+        write(file, "Dim oDesktop\n")
+        write(file, "Dim oProject\n")
+        write(file, "Dim oDesign\n")
+        write(file, "Dim oEditor\n")
+        write(file, "Dim oModule\n")
+        write(file, "Set oAnsoftApp = CreateObject(\"AnsoftHfss.HfssScriptInterface\")\n")
+        write(file, "Set oDesktop = oAnsoftApp.GetAppDesktop()\n")
+        write(file, "oDesktop.RestoreWindow\n")
+        write(file, "Set oProject = oDesktop.SetActiveProject(\"$(ProjectName)\")\n")
+        write(file, "Set oDesign = oProject.SetActiveDesign(\"HFSSDesign1\")\n")
+        write(file, "Set oEditor = oDesign.SetActiveEditor(\"3D Modeler\")\n")
+        write(file, "oEditor.CreatePolyline Array(\"NAME:PolylineParameters\", \"IsPolylineCovered:=\", true, \"IsPolylineClosed:=\", false,  _\n")
+        write(file, "Array(\"NAME:PolylinePoints\", _\n")
+        for i in 1:PointsNumber
+            x=X[i];
+            y=Y[i];
+            write(file, "Array(\"NAME:PLPoint\", \"X:=\", \"$(x)mm\", \"Y:=\", \"$(y)mm\", \"Z:=\", \"0mm\")")
+            if i != PointsNumber
+                write(file, ",  _\n")
+            end
+        end
+        write(file, "),  _\n")
+        write(file, "  Array(\"NAME:PolylineSegments\", _\n")
+        for i in 1:(PointsNumber-1)
+            write(file, "  Array(\"NAME:PLSegment\", \"SegmentType:=\", \"Line\", \"StartIndex:=\", $(i-1), \"NoOfPoints:=\", 2)")
+            if i != (PointsNumber-1)
+                write(file, ",  _\n")
+            end
+        end
+        write(file, "  ), _\n")
+        write(file, "  Array(\"NAME:PolylineXSection\", \"XSectionType:=\",  _\n")
+        write(file, "  \"None\", \"XSectionOrient:=\", \"Auto\", \"XSectionWidth:=\", \"0mm\", \"XSectionTopWidth:=\",  _\n")
+        write(file, "  \"0mm\", \"XSectionHeight:=\", \"0mm\", \"XSectionNumSegments:=\", \"0\", \"XSectionBendType:=\",  _\n")
+        write(file, "  \"Corner\")), Array(\"NAME:Attributes\", \"Name:=\", \"$(LineName)\", \"Flags:=\", \"\", \"Color:=\",  _\n")
+        write(file, "  \"(132 132 193)\", \"Transparency:=\", 0, \"PartCoordinateSystem:=\", \"Global\", \"UDMId:=\",  _\n")
+        write(file, "  \"\", \"MaterialValue:=\", \"\" & Chr(34) & \"vacuum\" & Chr(34) & \"\", _\n")
+        write(file, "  \"SolveInside:=\", true)\n")
+        close(file)
+    end
+end
